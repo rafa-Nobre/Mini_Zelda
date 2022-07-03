d11ca67c0f33f6a3cc0bbef39a5f7d6d1976e01d
donysRafa <50738060+donysRafa@users.noreply.github.com>
2022-07-03 09:52:12 -0300

    Incrementando Game.java

--- src/files/Game.java
+++ src/files/Game.java
@@ -1,5 +1,38 @@
 package files;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	public static int WIDTH = 480, HEIGHT = 480;
+	
+	public Game() {
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		JFrame frame = new JFrame();
+		
+		frame.add(game);
+		frame.setTitle("Mini Zelda");
+		
+		frame.pack();
+		
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+		
+		new Thread(game).start();
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			System.out.println("Running!");
+		}
+	}
 }
