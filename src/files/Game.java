bd66bcd259348814b3b14061bc5c9a0034f7be34
donysRafa <50738060+donysRafa@users.noreply.github.com>
2022-07-03 21:30:05 -0300

    Avançando na renderização!
    Implementação da parte gráfica e de colisões, assim como a adição do spritesheet para a parte visual.

--- src/files/Game.java
+++ src/files/Game.java
@@ -4,18 +4,29 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 	public static int WIDTH = 480, HEIGHT = 480;
 	
+	public Player player;
+	public World world;
+	
 	public Game() {
+		this.addKeyListener(this);
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+		new Spritesheet();
+		
+		player = new Player(50, 50);
+		world = new World();
 	}
 	
 	public void tick() {
-		
+		player.tick();
 	}
 	
 	public void render() {
@@ -31,8 +42,8 @@ public class Game extends Canvas implements Runnable {
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		
-		g.setColor(Color.red);
-		g.fillRect(0, 0, 50, 50);
+		player.render(g);
+		world.render(g);
 		
 		bs.show();
 	}
@@ -65,5 +76,45 @@ public class Game extends Canvas implements Runnable {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = true;
+		} else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			player.up = true;
+		}else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			player.down = true;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = false;
+		} else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			player.up = false;
+		}else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			player.down = false;
+		}
+		
 	}
 }
--- src/files/Player.java
+++ src/files/Player.java
@@ -6,6 +6,7 @@ import java.awt.Rectangle;
 
 public class Player extends Rectangle{
 	
+	public int spd = 6;
 	public boolean right, up, down, left;
 	
 	public Player(int x, int y) {
@@ -13,12 +14,23 @@ public class Player extends Rectangle{
 	}
 	
 	public void tick() {
+		if(right && World.IsFree(x + spd, y)) {
+			x += spd;
+		}else if(left && World.IsFree(x - spd, y)) {
+			x -= spd;
+		}
 		
+		if(up && World.IsFree(x, y - spd)) {
+			y -= spd;
+		}else if(down && World.IsFree(x, y + spd)){
+			y += spd;
+		}
 	}
 
 	
 	public void render(Graphics g) {
-		g.setColor(Color.blue);
-		g.fillRect(x, y, width, height);
+		//g.setColor(Color.blue);
+		//g.fillRect(x, y, width, height);
+		g.drawImage(Spritesheet.player_front, x, y, 32, 32, null);
 	}
 }
